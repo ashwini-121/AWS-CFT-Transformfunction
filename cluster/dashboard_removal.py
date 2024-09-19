0feaab14c815c5627b8e691eb84a494057316aa1
ashwini-121 <78910669+ashwini-121@users.noreply.github.com>
2024-09-19 20:42:09 +0530

    Add files via upload

---
+++ cluster/dashboard_removal.py
@@ -0,0 +1,43 @@
+import boto3
+import json
+import os
+from botocore.config import Config
+emr = boto3.client('emr')
+cloudwatch = boto3.client('cloudwatch')
+s3 = boto3.resource('s3')
+cloudwatchRes = boto3.resource('cloudwatch')
+metric = cloudwatchRes.Metric('namespace', 'name')
+paginator = cloudwatch.get_paginator('list_metrics')
+emr = boto3.client('emr')
+cloudwatch = boto3.client('cloudwatch')
+s3 = boto3.resource('s3')     
+custom_config = Config(
+    retries={
+        'max_attempts': 20, 
+        'mode': 'adaptive'
+    }
+)                      
+def handler(event, context):
+  clusterId=event['detail']['clusterId']
+  print(clusterId)
+  nameResponse = emr.describe_cluster(ClusterId=clusterId)
+  clusterName = nameResponse['Cluster']['Name']
+  clusterName=clusterName.split('-')[0]
+  name = clusterName+'-'+clusterId
+  paginator = cloudwatch.get_paginator('describe_alarms')
+  for response in paginator.paginate(AlarmNamePrefix=name):
+    # Do something with the alarm
+    print(response['MetricAlarms'])
+    AlarmName=[]
+    if response['MetricAlarms']:
+        for x in response['MetricAlarms']:
+            AlarmName.append(x['AlarmName'])
+            if( AlarmName):
+              print('deleting Alarms')
+              cloudwatch.delete_alarms(AlarmNames=AlarmName)
+        cloudwatch.delete_dashboards(DashboardNames=[name])
+  #cloudwatch.delete_alarms(AlarmNames=AlarmName)
+  #cloudwatch.delete_dashboards(DashboardNames=[name])      
+  print(AlarmName)
+  print(name)
+  return(AlarmName) 
